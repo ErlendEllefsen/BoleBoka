287f2338435ee26ed0bf4b927f8c7deb483f173c
VikingsCreed <ekbe_97@hotmail.no>
2020-09-22 13:02:38 +0200

    Bottom nav bar. Det er nå tre hovedfragmenter. Startactivity, stats, workouts. Navnene på navbar skal oppdateres LOL

--- app/src/main/java/com/example/boleboka/MainActivity.kt
+++ app/src/main/java/com/example/boleboka/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.boleboka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNav)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.startWorkout, R.id.stats, R.id.workouts))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
