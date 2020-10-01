f05e39b2fc50c22a3f2dad5d688deb5ae41346e8
VikingsCreed <ekbe_97@hotmail.no>
2020-10-01 11:59:32 +0200

    diverse styling

--- app/src/main/java/com/example/boleboka/Workouts.kt
+++ app/src/main/java/com/example/boleboka/Workouts.kt
@@ -10,16 +10,7 @@ import androidx.navigation.findNavController
 import com.example.boleboka.databinding.FragmentWorkoutsBinding
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Workouts.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Workouts : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
