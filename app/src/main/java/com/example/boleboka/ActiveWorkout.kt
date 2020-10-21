98df75c60562c40a08dac3a6c6c273ae896d9ced
VikingsCreed <ekbe_97@hotmail.no>
2020-10-21 19:26:12 +0200

    yo, la til sets som variabel i Exercise

--- app/src/main/java/com/example/boleboka/ActiveWorkout.kt
+++ app/src/main/java/com/example/boleboka/ActiveWorkout.kt
@@ -11,7 +11,7 @@ import kotlinx.android.synthetic.main.fragment_active_workout.*
 
 class ActiveWorkout : Fragment() {
 
-    private val testList = generateExersises(5)
+    private val exerciseList = generateExersises(5)
     private var i = 0
     private val resultsList = ArrayList<Result_Item>()
 
@@ -30,34 +30,30 @@ class ActiveWorkout : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        var currentList = testList[i]
-        var name = currentList.heading
-        var min = currentList.minReps
-        var max = currentList.maxReps
-        progressBar.progress += 100 / testList.size
-        setValues(name, min, max)
-
+        var currentList = exerciseList[i]
+        var name = currentList.name
+        var max = currentList.reps
+        progressBar.progress += 100 / exerciseList.size
+        setValues(name, max)
         btnBack.setOnClickListener() {
-            if (testList[i] == testList.first()) {
+            if (exerciseList[i] == exerciseList.first()) {
                 val startToast =
                     Toast.makeText(context, "This is the first exercise!", Toast.LENGTH_SHORT)
                 startToast.show()
             } else {
                 i -= 1
                 progress(false)
-                currentList = testList[i]
-                name = currentList.heading
-                min = currentList.minReps
-                max = currentList.maxReps
-                setValues(name, min, max)
+                currentList = exerciseList[i]
+                name = currentList.name
+                max = currentList.reps
+                setValues(name, max)
                 val listWeight = resultsList[i].weight
                 val listReps = resultsList[i].reps
                 setValuesFromList(listWeight, listReps)
             }
         }
-
         btnNext.setOnClickListener() {
-            if (testList[i] == testList.last()) {
+            if (exerciseList[i] == exerciseList.last()) {
                 val endToast =
                     Toast.makeText(context, "This is the last exercise!", Toast.LENGTH_SHORT)
                 endToast.show()
@@ -70,17 +66,16 @@ class ActiveWorkout : Fragment() {
                     storeValues(i)
                     i += 1
                     progress(true)
-                    currentList = testList[i]
-                    name = currentList.heading
-                    min = currentList.minReps
-                    max = currentList.maxReps
-                    setValues(name, min, max)
+                    currentList = exerciseList[i]
+                    name = currentList.name
+                    max = currentList.reps
+                    setValues(name, max)
                     if (resultsList.size > i) {
                         val listWeight = resultsList[i].weight
                         val listReps = resultsList[i].reps
                         setValuesFromList(listWeight, listReps)
                     }
-                    if (currentList == testList.last())
+                    if (currentList == exerciseList.last())
                         btnFinish.visibility = View.VISIBLE
                 }
             }
@@ -98,26 +93,24 @@ class ActiveWorkout : Fragment() {
         weight.setText(listWeight.toString())
     }
 
-
     private fun saveToDB() {
         // TODO: Lagre resultlist til databasen
         val list = Toast.makeText(context, resultsList.toString(), Toast.LENGTH_SHORT)
         list.show()
     }
 
-
     private fun progress(prog: Boolean) {
-        val divider = 100 / testList.size
+        val divider = 100 / exerciseList.size
         if (prog) {
             progressBar.progress += divider
         } else {
             progressBar.progress -= divider
         }
-        val list = Toast.makeText(context, progressBar.progress.toString(), Toast.LENGTH_SHORT)
-        list.show()
+        //  val list = Toast.makeText(context, progressBar.progress.toString(), Toast.LENGTH_SHORT).show()
+
     }
 
-    private fun setValues(name: String, min: Int, max: Int) {
+    private fun setValues(name: String, max: Int) {
         textView.text = name
         numberPicker.maxValue = max
         numberPicker.minValue = 0
@@ -137,15 +130,14 @@ class ActiveWorkout : Fragment() {
             } else
                 resultsList.add(pos, item)
         }
-
     }
 
-    private fun generateExersises(size: Int): ArrayList<Test_Item> {
+    private fun generateExersises(size: Int): ArrayList<Exercise_Item> {
 
-        val list = ArrayList<Test_Item>()
+        val list = ArrayList<Exercise_Item>()
 
         for (i in 0 until size) {
-            val item = Test_Item("Exersise $i", i, i + 2)
+            val item = Exercise_Item("Exersise $i", i + 5, i)
             list += item
         }
         return list
