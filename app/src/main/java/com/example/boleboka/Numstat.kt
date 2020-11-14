6a4f8295b7a418f82bf15ab3759bb203d0fcd69b
Daniel Jansson <141276.hit@gmail.com>
2020-11-14 12:47:19 +0100

    nav+btn numstat

--- app/src/main/java/com/example/boleboka/Numstat.kt
+++ app/src/main/java/com/example/boleboka/Numstat.kt
@@ -6,12 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import com.google.android.material.datepicker.MaterialDatePicker
+import kotlinx.android.synthetic.main.fragment_active_workout.*
 import kotlinx.android.synthetic.main.fragment_numstat.*
 import java.util.*
 
 class Numstat : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+
+
+    }
+
 
 
     override fun onCreateView(
