23e8a9dd0bb18fa54ff39e76a6017069efebacc8
VikingsCreed <ekbe_97@hotmail.no>
2020-11-27 12:07:04 +0100

    landscapemode for alle fragmenter

--- app/src/main/java/com/example/boleboka/Communicator.kt
+++ app/src/main/java/com/example/boleboka/Communicator.kt
@@ -1,18 +1,11 @@
 package com.example.boleboka
 
-import android.widget.Spinner
-import androidx.core.view.get
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 /*
  * Her legges informasjon som skal deles mellom fragment
  * Nå brukes den bare til å dele info fra workouts til exersise
- */
-
-/*
- * TODO: Jon, her kan man dele ID fra workout til exersise. Da vet exersise
- *  hvilken exersiseliste den skal displaye.
  */
 class Communicator : ViewModel(){
     val message = MutableLiveData<Any>()
@@ -25,6 +18,4 @@ class Communicator : ViewModel(){
     fun positionCommunicator(pos: Int) {
         position.value = pos
     }
-
-
 }
